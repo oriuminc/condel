26bb744c4dee628e25827084c75bcbb6ca76e885
Patrick Connolly <patrick@myplanetdigital.com>
2012-03-10 17:07:13 -0500

    Cleaned up Varnish snippet.

--- includes/apc.settings.php
+++ includes/apc.settings.php
@@ -6,7 +6,9 @@
  * Add APC Caching.
  */
 $conf['cache_backends'][] = 'profiles/condel/modules/contrib/apc/drupal_apc_cache.inc';
-// $conf['apc_show_debug'] = TRUE;  // Remove the slashes to use debug mode.
+
+// Remove slashes to use debug mode.
+// $conf['apc_show_debug'] = TRUE;
 
 // Array of Drupal cache bins to cache with APC
 $drupal_cache_bins_apc = array(
--- includes/varnish.settings.php
+++ includes/varnish.settings.php
@@ -5,9 +5,18 @@
 /**
  * Add Varnish Caching.
  */
-// Add Varnish as the page cache handler.
 $conf['cache_backends'][] = 'profiles/condel/modules/contrib/varnish/varnish.cache.inc';
-$conf['cache_class_cache_page'] = 'VarnishCache';
+
 // Drupal 7 does not cache pages when we invoke hooks during bootstrap. This needs
 // to be disabled.
 $conf['page_cache_invoke_hooks'] = FALSE;
+
+// Array of Drupal cache bins to cache with Varnish
+$drupal_cache_bins_varnish = array(
+  'cache_page',
+);
+
+foreach ($drupal_cache_bins_varnish as $cache_bin) {
+  // Assign each Drupal cache bin to Varnish caching
+  $conf['cache_class_' . $cache_bin] = 'VarnishCache';
+}
